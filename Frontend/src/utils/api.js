189defbf27a2b6b6be24a765dc4699d5b0edec78
raj <praj212004@gmal.com>
2026-08-26 23:54:41 +0530

    render

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://cafe-management-pord.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
